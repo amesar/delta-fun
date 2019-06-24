42fdacb549dc0b2ef7a5eaaa070edf2ff5a69836
amesar <amesar@users.noreply.github.co>
2019-06-24 18:03:57 +0000

    removed db notebooks stuff from time_travel.py

--- python/time_travel.py
+++ python/time_travel.py
@@ -13,56 +13,42 @@ print("Python version:",sys.version)
 
 in_databricks = 'DATABRICKS_RUNTIME_VERSION' in os.environ
 print("in_databricks:",in_databricks)
-table = "cats"
 
 def insert(path, data, mode="append"):
     df = spark.createDataFrame(data, ["id","name","region"])
     df.coalesce(1).write.mode(mode).format("delta").save(path)
 
-def init():
-    if in_databricks:
-        user = dbutils.notebook.entry_point.getDbutils().notebook().getContext().tags().get("user").get()
-        user = user.split("@")[0].replace(".","_")
-        database = "{}_delta_fun".format(user)
-        dataPath = "dbfs:/tmp/{}/delta_fun/table_cats".format(user)
-    else:
-        database = "delta_fun"
-        dataPath = "delta_fun"
-    return (database,dataPath)
-
-
-def main():
-    database,dataPath = init()
+def main(data_path, database, table):
+    print("data_path:",data_path)
     print("database:",database)
     print("table:",table)
-    print("dataPath:",dataPath)
 
-    insert(dataPath, [
+    insert(data_path, [
         (1, "lion","africa"),
         (2, "cheetah","africa"),
         (3, "leopard","africa")], "overwrite")
-    insert(dataPath, [
+    insert(data_path, [
         (4, "jaguar","south america"),
         (5, "puma","south america"),
         (6, "ocelot","south america")])
-    insert(dataPath, [
+    insert(data_path, [
         (7, "lynx","north america"),
         (8, "bobcat","north america"),
         (9, "catamount","north america")])
 
     print("Data Files:")
-    files = dbutils.fs.ls(dataPath) if in_databricks else os.listdir(dataPath)
+    files = dbutils.fs.ls(data_path) if in_databricks else os.listdir(data_path)
     for f in files:
         print("  ",f)
 
     print("All data:")
-    df = spark.read.format("delta").load(dataPath)
+    df = spark.read.format("delta").load(data_path)
     df.sort("id").show()
 
     print("Data Versions:")
     for v in range(0,3):
         print("Version:",v)
-        df = spark.read.format("delta").option("versionAsOf", str(v)).load(dataPath)
+        df = spark.read.format("delta").option("versionAsOf", str(v)).load(data_path)
         df.sort("id").show(100,False)
 
     print("==== SQL")
@@ -71,7 +57,7 @@ def main():
     spark.sql("drop table if exists {}".format(table))
     df = spark.sql("show databases").filter("databaseName = '{}'".format(database))
 
-    spark.sql("create table {} using delta location '{}'".format(table,dataPath))
+    spark.sql("create table {} using delta location '{}'".format(table,data_path))
     spark.sql("describe formatted {}".format(table)).show(1000,False)
 
     print("All data:")
@@ -85,5 +71,10 @@ def main():
             print("Version:",v)
             spark.sql("select * from {} version as of {} order by id".format(table,v)).show()
 
+    print("Everything ran OK")
+
 if __name__ == "__main__":
-    main()
+    data_path = sys.argv[1] if len(sys.argv) > 1 else "delta_fun"
+    database = sys.argv[2] if len(sys.argv) > 2 else "delta_fun"
+    table = sys.argv[3] if len(sys.argv) > 3 else "cats"
+    main(data_path,database,table)
